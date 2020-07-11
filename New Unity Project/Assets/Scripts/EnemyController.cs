1177642f2437ef047e84ea1f66a0c4febcf14cba
MatheusCapucho <matheusfcapucho@gmail.com>
2020-07-11 02:14:56 -0300

    [ADD] Basic Movement

--- New Unity Project/Assets/Scripts/EnemyController.cs
+++ New Unity Project/Assets/Scripts/EnemyController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject player;
+    private Rigidbody2D rb; 
+    private float speed = 10f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(-speed, rb.velocity.y);
+    }
+    private void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //perde vida ou morre 
+        }
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+            speed = -speed;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   
+    private float movementInput;
+    private float speed = 5f;
+    private float jumpForce = 10f;
+    private float dashForce;
+    private Rigidbody2D rb;
+    public Transform attackPoint; 
+    public Vector2 playerPos;
+    bool isGrounded = true;
+    bool trocar;
+    int weaponIndex = 0;
+    float weaponRange = 10f;
+    bool isFacingRight;
+
+    private void Awake()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        trocar = true;
+    }
+    private void start()
+    {
+        isFacingRight = true;
+        
+    }
+
+    void Update()
+    {
+        movementInput = Input.GetAxisRaw("Horizontal");
+
+        //flip
+        if (isFacingRight && movementInput * speed < 0)
+        {
+            isFacingRight = false;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+        if (!isFacingRight && movementInput * speed > 0)
+        {
+            isFacingRight = true;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+
+        //attack
+        if (Input.GetKeyDown(KeyCode.F)) 
+        {
+            Attack();
+        }
+
+        //pular ou dash - trocando
+        if (trocar)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rb.velocity = Vector2.up * jumpForce;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {    
+                StartCoroutine(Dash());     
+            }
+        } else
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                StartCoroutine(Dash()); 
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.velocity = Vector2.up * jumpForce;
+            }
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movementInput * (speed + dashForce), rb.velocity.y);
+    }
+
+    private void Attack()
+    {
+        Physics2D.Raycast(playerPos, Vector2.right, weaponRange, 8);
+        //animacao
+        //mudar o colider
+        //diferentes armas (switch-case)
+        //if(enemyKilled) {  trocar = !trocar; speed = -speed; dashForce = -dashForce; }
+    }
+    IEnumerator Dash()
+    {
+        dashForce = 10f;
+        yield return new WaitForSeconds(0.2f);
+        dashForce = 0f;
+    }
+
+}
